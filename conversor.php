59b77b082a301c31d0f22ef8825ddf4a62bdd201
Alex Junio <48035157+AlexJunio07@users.noreply.github.com>
2020-12-28 13:30:13 -0300

    commit

---
+++ conversor.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/config.js"></script>
+</head>
+
+<body>
+    <?php
+
+    function extenso($valor = 0, $maiusculas = false)
+    {
+        if (!$maiusculas) {
+            $singular = ["centavo", "real", "mil", "milhão", "bilhão", "trilhão", "quatrilhão"];
+            $plural = ["centavos", "reais", "mil", "milhões", "bilhões", "trilhões", "quatrilhões"];
+            $u = ["", "um", "dois", "três", "quatro", "cinco", "seis",  "sete", "oito", "nove"];
+        } else {
+            $singular = ["CENTAVO", "REAL", "MIL", "MILHÃO", "BILHÃO", "TRILHÃO", "QUADRILHÃO"];
+            $plural = ["CENTAVOS", "REAIS", "MIL", "MILHÕES", "BILHÕES", "TRILHÕES", "QUADRILHÕES"];
+            $u = ["", "um", "dois", "TRÊS", "quatro", "cinco", "seis",  "sete", "oito", "nove"];
+        }
+
+        $c = ["", "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"];
+        $d = ["", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"];
+        $d10 = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove"];
+
+        $z = 0;
+        $rt = "";
+
+        $valor = number_format($valor, 2, ".", ".");
+        $inteiro = explode(".", $valor);
+        for ($i = 0; $i < count($inteiro); $i++)
+            for ($ii = strlen($inteiro[$i]); $ii < 3; $ii++)
+                $inteiro[$i] = "0" . $inteiro[$i];
+
+        $fim = count($inteiro) - ($inteiro[count($inteiro) - 1] > 0 ? 1 : 2);
+        for ($i = 0; $i < count($inteiro); $i++) {
+            $valor = $inteiro[$i];
+            $rc = (($valor > 100) && ($valor < 200)) ? "cento" : $c[$valor[0]];
+            $rd = ($valor[1] < 2) ? "" : $d[$valor[1]];
+            $ru = ($valor > 0) ? (($valor[1] == 1) ? $d10[$valor[2]] : $u[$valor[2]]) : "";
+
+            $r = $rc . (($rc && ($rd || $ru)) ? " e " : "") . $rd . (($rd &&
+                $ru) ? " e " : "") . $ru;
+            $t = count($inteiro) - 1 - $i;
+            $r .= $r ? " " . ($valor > 1 ? $plural[$t] : $singular[$t]) : "";
+            if ($valor == "000") $z++;
+            elseif ($z > 0) $z--;
+            if (($t == 1) && ($z > 0) && ($inteiro[0] > 0)) $r .= (($z > 1) ? " de " : "") . $plural[$t];
+            if ($r) $rt = $rt . ((($i > 0) && ($i <= $fim) && ($inteiro[0] > 0) && ($z < 1)) ? (($i < $fim) ? ", " : " e ") : " ") . $r;
+        }
+
+        if (!$maiusculas) {
+            $return = $rt ? $rt : "zero";
+        } else {
+            if ($rt) $rt = preg_replace("[^A-Za-z0-9]", "", ucwords($rt));
+            $return = ($rt) ? ($rt) : "Zero";
+        }
+
+        if (!$maiusculas) {
+            return preg_replace("[^A-Za-z0-9]", "", ucwords($return));
+        } else {
+            return strtoupper($return);
+        }
+    }
+    
+    $numero = $_GET["valor"];
+    $extenso = strtoupper(extenso($numero));
+    print json_encode($numero."<br>". $extenso);
+    ?>
+
+
+
+</body>
+
+</html>
